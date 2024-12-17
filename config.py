615866ef9b037ef387db9573348bfcdb903c0068
Dominic Ang <zenpaiang@gmail.com>
2024-12-17 16:08:44 +0800

    remove ALLIANCE_TAG configuration value

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
 class Config:
     BOT_TOKEN = "" # enter your bot token here
-    ALLIANCE_NAME = "" # enter your alliance name (3 letter code) here
     PLAYERS_FILE = "players.json" # path to players database
