46f8a3110f40f83edae5986b8d095c8134cba0ae
Dominic Ang <zenpaiang@gmail.com>
2024-12-23 22:02:41 +0800

    add status message support in config

--- bot.py
+++ bot.py
@@ -4,6 +4,11 @@ import config
 client = discord.Client(intents=discord.Intents.GUILDS)
 
 client.config = config.Config()
+
+@discord.listen()
+async def on_ready():
+    if client.config.ACTIVITY:
+        await client.change_presence(activity=client.config.ACTIVITY)
     
 client.load_extensions("cogs")
     
--- config.example.py
+++ config.example.py
@@ -1,3 +1,9 @@
+import interactions as discord
+
 class Config:
     BOT_TOKEN = "" # enter your bot token here
     PLAYERS_FILE = "players.json" # path to players database
+    ACTIVITY = discord.Activity(
+        name="example activity",
+        type=discord.ActivityType.PLAYING,
+    )
