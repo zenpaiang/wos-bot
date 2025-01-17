12f00c76864a1d620527f58e914c12ffb81250d2
Dominic Ang <zenpaiang@gmail.com>
2025-01-17 18:43:54 +0800

    add auto rename system

--- cogs/giftcode.py
+++ cogs/giftcode.py
@@ -46,7 +46,7 @@ def match_score(item: str, against: str) -> float:
 
 class Giftcode(discord.Extension):
     def __init__(self, bot: discord.Client):
-        self.redeemLimits = {"inUse": False, "lastUse": 0}
+        self.apiLimits = {"inUse": False, "lastUse": 0}
         self.bot = bot
     
     async def redeem_code(self, session: aiohttp.ClientSession, code: str, player: dict) -> tuple[bool, str, str]:
@@ -158,6 +158,126 @@ class Giftcode(discord.Extension):
             counters=counters,
             recursive_depth=recursive_depth + 1,
         )
+        
+    async def rename_player(self, session: aiohttp.ClientSession, player: dict) -> tuple[bool, str, str]:
+        timens = time.time_ns()
+        
+        login_resp = await session.post(
+            url="https://wos-giftcode-api.centurygame.com/api/player",
+            data={
+                "fid": player["id"],
+                "time": timens,
+                "sign": hashlib.md5(f"fid={player['id']}&time={timens}tB87#kPtkxqOS2".encode("utf-8")).hexdigest()
+            },
+            headers={
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Accept": "application/json",
+            },
+            timeout=30
+        )
+        
+        try:
+            login_result = await login_resp.json()
+        except Exception as _:
+            return False, "error", "login error"
+        
+        if "msg" in login_result:
+            if login_result["msg"] != "success":
+                return False, "error", "login error"
+            else:
+                if sanitize_username(login_result["data"]["nickname"]) != player["name"]:
+                    with open(self.bot.config.PLAYERS_FILE, "r") as f:
+                        players = json.load(f)
+                        
+                    players[player["id"]]["name"] = sanitize_username(login_result["data"]["nickname"])
+                    
+                    print(f"renamed {player['name']} to {login_result['data']['nickname']}")
+                    
+                    with open(self.bot.config.PLAYERS_FILE, "w") as f:
+                        json.dump(players, f, indent=4)
+                        
+                    return False, "renamed", "none"
+                else:
+                    return False, "success", "none"
+        else:
+            return True, None, "rate limited"
+        
+    async def recursive_rename(self, message: discord.Message, session: aiohttp.ClientSession, players: list, counters: dict = {"renamed": 0, "success": 0, "error": 0}, recursive_depth: int = 0):
+        results = {}
+        
+        for i in range(0, len(players), 20):
+            batch = players[i:i + 20]
+            
+            msg = "renaming users" if recursive_depth == 0 else f"renaming users (retry {recursive_depth})"
+            
+            await message.edit(content=f"{msg}... ({min(i, len(players))}/{len(players)}) | next update <t:{1 + int(time.time()) + (len(batch) * 3)}:R>")
+            
+            for player in batch:
+                start = time.time()
+                
+                exit, counter, result = await self.rename_player(session, player)
+                
+                if exit:
+                    await message.edit(content=f"error: {result}")
+                    return
+                else:
+                    counters[counter] += 1
+                    results[player["name"]] = result
+                    
+                await asyncio.sleep(max(0, 3 - (time.time() - start)))
+                    
+        remaining_players = [player for player in players if "error" in results[player["name"]]]
+        
+        if len(remaining_players) == 0:
+            msg = (
+                f"report: rename users\n"
+                f"renamed: {counters['renamed']} | "
+                f"nothing: {counters['success']} | "
+                f"retries: {recursive_depth}\n\n"
+                f"made with ❤️ by zenpai :D"
+            )
+            
+            await message.edit(content=msg)
+            
+            await session.close()
+            return
+                    
+        await self.recursive_redeem(
+            message=message,
+            session=session,
+            players=remaining_players,
+            counters=counters,
+            recursive_depth=recursive_depth + 1,
+        )
+        
+    @discord.slash_command(
+        name="giftcode",
+        description="giftcode-related commands",
+        sub_cmd_name="autorename",
+        sub_cmd_description="perform auto renaming of users"
+    )
+    async def autorename(self, ctx: discord.SlashContext):
+        if self.apiLimits["inUse"]:
+            await ctx.send("error: there can only be one instance of this command running at once")
+            return
+        
+        if self.apiLimits["lastUse"] + 60 > time.time():
+            await ctx.send("error: this command has a limit of 1 use every 1 minute to comply with WOS's rate limits")
+            return
+        
+        with open(self.bot.config.PLAYERS_FILE, "r") as f:
+            playersObj = json.load(f)
+            
+        players = [{"id": key, "name": playersObj[key]["name"]} for key in playersObj]
+        
+        session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=ssl.create_default_context(cafile=certifi.where())))
+        
+        message = await ctx.send("waiting...")
+        
+        await self.recursive_rename(message, session, players)
+        
+        self.apiLimits["inUse"] = False
+        self.apiLimits["lastUse"] = time.time()
     
     @discord.slash_command(
         name="giftcode",
@@ -174,11 +294,11 @@ class Giftcode(discord.Extension):
         ]
     )
     async def redeem(self, ctx: discord.SlashContext, code: str):
-        if self.redeemLimits["inUse"]:
+        if self.apiLimits["inUse"]:
             await ctx.send("error: there can only be one instance of this command running at once")
             return
         
-        if self.redeemLimits["lastUse"] + 60 > time.time():
+        if self.apiLimits["lastUse"] + 60 > time.time():
             await ctx.send("error: this command has a limit of 1 use every 1 minute to comply with WOS's rate limits")
             return
         
@@ -193,8 +313,8 @@ class Giftcode(discord.Extension):
         
         await self.recursive_redeem(message, session, code, players)
         
-        self.redeemLimits["inUse"] = False
-        self.redeemLimits["lastUse"] = time.time()
+        self.apiLimits["inUse"] = False
+        self.apiLimits["lastUse"] = time.time()
     
     @discord.slash_command(
         name="giftcode",
