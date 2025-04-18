ca7854dd84bf8798878e2e74c8db8d86da34e1f9
Dominic Ang <zenpaiang@gmail.com>
2025-04-18 23:04:54 +0800

    removed unnecessary files

--- config.example.py
+++
@@ -1,9 +0,0 @@
-import interactions as discord
-
-class Config:
-    BOT_TOKEN = "" # enter your bot token here
-    PLAYERS_FILE = "players.json" # path to players database
-    ACTIVITY = discord.Activity(
-        name="example activity",
-        type=discord.ActivityType.PLAYING
-    )
