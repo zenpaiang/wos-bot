e3c2809bd3e5af4de6c9bbc9c2afefb33ce6a5b4
Dominic Ang <zenpaiang@gmail.com>
2025-04-19 21:13:04 +0800

    new bot code

--- bot.py
+++ bot.py
@@ -1,39 +1,18 @@
 import interactions as discord
-import config
 import json
+import cfg
 
-client = discord.Client(intents=discord.Intents.GUILD_MEMBERS | discord.Intents.GUILDS)
+class CustomClient(discord.Client):   
+    config: cfg.Config = None
+     
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        with open("config.json", "r") as f:
+            self.config: cfg.Config = cfg.Config.from_dict(json.load(f))
 
-client.config = config.Config()
+client = CustomClient(intents=discord.Intents.GUILDS)
 
-# checker + migrator (will be removed in future update)
-
-with open(client.config.PLAYERS_FILE, "r") as f:
-    players = json.load(f)
-    
-    if players:
-        if not isinstance(players[list(players.keys())[0]], dict):
-            print("migrating to v2 format...")
-            
-            new_players = {}
-
-            for uid in players:
-                new_players[uid] = {
-                    "name": players[uid],
-                    "rank": 0
-                }
-                
-            new = open("players.json", "w")
-            json.dump(new_players, new, indent=4)
-            new.close()
-            
-            print("successfully migrated to v2")
-
-@discord.listen()
-async def on_ready():
-    if client.config.ACTIVITY:
-        await client.change_presence(activity=client.config.ACTIVITY)
-    
 client.load_extensions("cogs")
     
-client.start(client.config.BOT_TOKEN)
+client.start(client.config.token)
